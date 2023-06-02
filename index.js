aee46c18b4e28884911502b80e5e35e1d0bd7419
Aldhair Vera Camacho <aldhairvera15@gmail.com>
2023-06-02 17:29:01 -0500

    Update routes to get ids from request params

--- index.js
+++ index.js
@@ -2,15 +2,41 @@ const express = require("express");
 const app=express();
 const PORT = 3000;
 
+let products = [
+  {
+    id:0,
+    name: "Product1",
+    price: 1000
+  },
+  {
+    id:1,
+    name: "Product2",
+    price: 2000
+  },
+]
+
 app.get("/",(req,res)=>{
   res.send("Hi from my express server")
 })
 
-app.get("/new",(req,res)=>{
-  res.json({
-    name: "Aldhair",
-    price: 1000
-  })
+app.get("/products",(req,res)=>{
+  res.json(products)
+})
+
+app.get("/products/:id",(req,res)=>{
+  const {id} = req.params;
+  const product = products.filter(product=>product.id.toString()===id)[0]
+  console.log("Log",{id, product})
+  res.json(product)
+})
+
+app.get("/categories/:categoryId/products/:productId",(req,res)=>{
+  const {categoryId, productId} = req.params;
+  const product = {
+    categoryId,
+    productId
+  }
+  res.json(product)
 })
 
 app.listen(PORT,()=>{

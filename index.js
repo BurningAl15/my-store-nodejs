00e27fc36bdee6886d0e7b1e33013fedda232319
Aldhair Vera Camacho <aldhairvera15@gmail.com>
2023-06-02 19:07:32 -0500

    Using request query

--- index.js
+++ index.js
@@ -2,32 +2,40 @@ const express = require("express");
 const app=express();
 const PORT = 3000;
 
-let products = [
-  {
-    id:0,
-    name: "Product1",
-    price: 1000
-  },
-  {
-    id:1,
-    name: "Product2",
-    price: 2000
-  },
-]
+const {faker} = require("@faker-js/faker");
 
 app.get("/",(req,res)=>{
   res.send("Hi from my express server")
 })
 
 app.get("/products",(req,res)=>{
+  const products=[];
+  const {size} = req.query;
+  const limit = size || 10;
+  for(let index=0;index<limit;index++){
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price()),
+      image: faker.image.imageUrl()
+    })
+  }
   res.json(products)
+})
+
+app.get("/products/filter",(req,res)=>{
+  res.send("Im a filter")
 })
 
 app.get("/products/:id",(req,res)=>{
   const {id} = req.params;
-  const product = products.filter(product=>product.id.toString()===id)[0]
-  console.log("Log",{id, product})
-  res.json(product)
+  // const product = products.filter(product=>product.id.toString()===id)[0]
+  // console.log("Log",{id, product})
+  // res.json(product)
+  res.json({
+    id,
+    name: "Product 2",
+    price: 2000
+  })
 })
 
 app.get("/categories/:categoryId/products/:productId",(req,res)=>{
@@ -37,6 +45,19 @@ app.get("/categories/:categoryId/products/:productId",(req,res)=>{
     productId
   }
   res.json(product)
+})
+
+app.get("/users",(req,res)=>{
+  const {limit,offset} = req.query;
+  if(limit && offset){
+    res.json({
+      limit,
+      offset
+    })
+  }
+  else{
+    res.send("No parameters")
+  }
 })
 
 app.listen(PORT,()=>{
